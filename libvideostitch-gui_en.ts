ed8e2f6fb4ca7f12308f364ca0c6c2e9fd779077
Jean Duthon <duthon.jean@gmail.com>
2014-08-12 10:48:27 +0200

    Update libvideostitch-gui_en.ts

--- libvideostitch-gui_en.ts
+++ libvideostitch-gui_en.ts
@@ -5,7 +5,7 @@
     <name>BasicMpegCodec</name>
     <message>
         <location filename="../../libvideostitch-gui/centralwidget/formats/codecs/basicmpegcodec.cpp" line="28"/>
-        <source>Bitrate Mode</source>
+        <source>Bitrate Mod</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
