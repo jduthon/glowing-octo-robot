9028fb65b997a108dead5adb408f22801e16036b
buildbot <buildbot@buildbot-master.(none)>
2014-08-21 14:25:30 +0200

    test

---
+++ test_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>BasicMpegCodec</name>
+    <message>
+        <location filename="../../libvideostitch-gui/centralwidget/formats/codecs/basicmpegcodec.cpp" line="28"/>
+        <source>Bitrate Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../libvideostitch-gui/centralwidget/formats/codecs/basicmpegcodec.cpp" line="29"/>
+        <source>Bitrate (kbps)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
