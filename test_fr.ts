d19748a526fc938ab23ae2960fff4bba36ce3545
Jean Duthon <duthon.jean@gmail.com>
2014-08-22 09:47:50 +0200

    Updating translations for test_fr.ts

---
+++ test_fr.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
+<context>
+    <name>BasicMpegCodec</name>
+    <message>
+        <location filename="../../libvideostitch-gui/centralwidget/formats/codecs/basicmpegcodec.cpp" line="28"/>
+        <source>Bitrate Mode</source>
+        <translation>test</translation>
+    </message>
+    <message>
+        <location filename="../../libvideostitch-gui/centralwidget/formats/codecs/basicmpegcodec.cpp" line="29"/>
+        <source>Bitrate (kbps)</source>
+        <translation>B (s)</translation>
+    </message>
+</context>
+</TS>
